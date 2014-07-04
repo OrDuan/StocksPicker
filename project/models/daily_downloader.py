c0a725048bd8a9c4560b1d0e48e709eed49df635
Or Duan <orduani@gmail.com>
2014-07-04 17:34:39 +0300

    Fixed threading

--- project/models/daily_downloader.py
+++ project/models/daily_downloader.py
@@ -20,23 +20,18 @@ def daily_looper(start, end):
         for symbol in symbols[start:]:
             daily_downloader_get_data(symbol)
 
+
 def daily_downloader_get_data(symbol):
     try:
         url_string = "http://www.google.com/finance/getprices?q=" + symbol + "&i=86400&p=1d&f=d,c,h,l,o,v"
         list_of_datas[symbol] = [urllib2.urlopen(url_string).readlines()]
-        # History(symbol=symbol,
-        #       date=,
-        #       high=,
-        #       low=,
-        #       open=,
-        #       close=,
-        #       volume=,)
         print "Downloaded list_of_datas for " + symbol
     except:
         print "@@@@@@@@Failed to download data for " + symbol + "@@@@@@@@"
         print "Trying again in 5 secs..."
         sleep(5)
         daily_downloader_get_data(symbol)
+
 
 def save_data(symbol):
     try:
@@ -76,21 +71,21 @@ def save_data(symbol):
 def run_daily_downloader():
     time0 = time()
     # Download last year data
-    t1 = threading.Thread(target=daily_looper, args=(0, 5))
-    # t2 = threading.Thread(target=looper, args=(101, 200))
-    # t3 = threading.Thread(target=looper, args=(201, 300))
-    # t4 = threading.Thread(target=looper, args=(301, 400))
-    # t5 = threading.Thread(target=looper, args=(401, 0))
+    t1 = threading.Thread(target=daily_looper, args=(0, 100))
+    t2 = threading.Thread(target=daily_looper, args=(101, 200))
+    t3 = threading.Thread(target=daily_looper, args=(201, 300))
+    t4 = threading.Thread(target=daily_looper, args=(301, 400))
+    t5 = threading.Thread(target=daily_looper, args=(401, 0))
     t1.start()
-    # t2.start()
-    # t3.start()
-    # t4.start()
-    # t5.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
     t1.join()
-    # t2.join()
-    # t3.join()
-    # t4.join()
-    # t5.join()
+    t2.join()
+    t3.join()
+    t4.join()
+    t5.join()
 
     # Save the data into the db
     for save_symbol in symbols:
